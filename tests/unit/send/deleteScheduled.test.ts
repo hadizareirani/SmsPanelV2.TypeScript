8279b83fcd25a65dcf042d52617540eb149c3d54
hadi zare irani <22741265+hadizareirani@users.noreply.github.com>
2025-12-06 16:15:51 +0330

    Feat/unit test (#3)
    * chore: remove empty test files from unit tests directory
    
    * feat: add unit tests for Smsir class and smsBuilder function
    
    * fix: update test descriptions for Smsir class and smsBuilder function to reflect multiple sendSms methods
    
    * feat: add unit tests for sendBulk method in Smsir class
    
    * feat: expand unit tests for sendBulk method in Smsir class to cover additional scenarios
    
    * feat: add unit tests for deleteScheduled method in Smsir class
    
    * feat: clean up unit tests for sendBulk method in Smsir class by removing redundant tests

---
+++ tests/unit/send/deleteScheduled.test.ts
@@ -0,0 +1,64 @@
+import {describe, test, expect, beforeEach, vi, afterEach} from 'vitest'
+import { Smsir } from '../../../src/index';
+import { ok } from 'assert';
+
+describe("Smsir Class - deleteScheduled Method",()=> {
+    let smsir: Smsir;
+    
+    beforeEach(()=> {
+        smsir = new Smsir('test-api-key', 30007732000000);
+        vi.clearAllMocks();
+    });
+
+    afterEach(() => {
+        vi.restoreAllMocks();
+    });
+
+    test("should have deleteScheduled method",() => {
+        expect(typeof smsir.deleteScheduled).toBe('function');
+    });
+
+    test("deleteScheduled method should return a Promise",() => {
+        global.fetch = vi.fn().mockResolvedValue({
+            ok: true,
+            json: async ()=>({ returnedCreditCount: 10, smsCount: 5 })
+        })
+
+        const result = smsir.deleteScheduled("12345");
+        expect(result).toBeInstanceOf(Promise)
+    });
+
+    test("deleteScheduled method should return the correct response", async () => {
+          const mockResponse = {
+            status: 1,
+            message: "عملیات با موفقیت انجام شد",
+            data: { returnedCreditCount: 10, smsCount: 5 }
+        };
+        global.fetch = vi.fn().mockResolvedValue({
+            ok: true,
+            json: async () => mockResponse
+        })
+
+        const result = await smsir.deleteScheduled("12345");
+
+
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith(
+            expect.stringContaining('/v1/send/scheduled/12345'),
+            expect.objectContaining({
+                method: 'DELETE',
+                headers: expect.objectContaining({
+                    'X-API-KEY': 'test-api-key',
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                })
+            })
+        );
+
+        expect(result).toEqual(mockResponse);
+        expect(result.data.returnedCreditCount).toBe(10);
+        expect(result.data.smsCount).toBe(5);
+    })
+
+    
+});
