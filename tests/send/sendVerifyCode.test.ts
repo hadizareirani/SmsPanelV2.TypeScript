0813affc2142831e55c61f073bb3420a2a813c20
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2025-12-09 11:44:31 +0330

    feat: remove obsolete sendVerifyCode test file

--- tests/send/sendVerifyCode.test.ts
+++
@@ -1,7 +0,0 @@
-import {describe, test, expect} from 'vitest'
-import { setupSmsirTest, createMockResponse, mockFetchSuccess } from '../setup/testUtils';
-
-
-describe("Smsir Class - sendVerifyCode Method",()=> {
-    const { getSmsir } = setupSmsirTest();
-});
